646d523670fd1a065415480c03b0febd7ea81bc9
Akshat Chourey <akshat.chourey@cred.club>
2024-10-10 17:46:10 +0530

    added test report

--- src/main/java/utils/TestReportUtils.java
+++ src/main/java/utils/TestReportUtils.java
@@ -5,17 +5,19 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 public class TestReportUtils {
     private static ExtentTest test;
     private static final String REPORT_PATH = "target/extent-reports/extent.html";
-    private static ExtentReports extent;
+    private static ExtentReports extent ;
     public static void initReport(){
         ExtentSparkReporter htmlReporter = new ExtentSparkReporter(REPORT_PATH);
-        ExtentReports extent = new ExtentReports();
+        extent =new ExtentReports();
         extent.attachReporter(htmlReporter);
     }
     public static ExtentTest createTest(String testName) {
         test = extent.createTest(testName);
         return test;
     }
-
+    public static void flushReport() {
+        extent.flush();
+    }
     public static String getReportPath() {
         return REPORT_PATH;
     }
--- src/test/java/assignment/Fancode.java
+++ src/test/java/assignment/Fancode.java
@@ -1,67 +1,99 @@
 package assignment;
 
 import api.TypeicodeAPIS;
+import com.epam.ta.reportportal.ws.annotations.In;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.util.List;
 import java.util.Map;
-
+import com.aventstack.extentreports.ExtentTest;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import utils.TestReportUtils;
 
 public class Fancode{
-
+    private ExtentTest test;
     public TypeicodeAPIS typeicodeAPIS = new TypeicodeAPIS();
+    List<Integer> userIds;
 
+    @BeforeClass
+    public void setUpClass() {
+        TestReportUtils.initReport();
+
+    }
     @Test(priority = 1)
     public void checkUsersApi(){
+        test =TestReportUtils.createTest("Checking Users Api");
         Integer respCode = typeicodeAPIS.checkUsersApi();
+        test.info("Status Code received:" + respCode);
         Assert.assertEquals(respCode,200,"Users api connection is stable");
     }
 
     @Test(priority = 1)
     public void checkTodisApi(){
+        test = TestReportUtils.createTest("Checking Todos Api");
         Integer respCode = typeicodeAPIS.checkTodosApi();
+        test.info("Status Code received:" + respCode);
         Assert.assertEquals(respCode,200,"Todos Api connection is stable");
     }
 
     @Test(priority = 2)
     public void getEligibleUsers() {
-
-        Assert.assertFalse(typeicodeAPIS.getEligibleUsersIds().isEmpty(),"No users found in Fancode city");
+        test = TestReportUtils.createTest("Finding users belonging to Fancode City given the lat long conditions.");
+        List<Integer> userIds = typeicodeAPIS.getEligibleUsersIds();
+        this.userIds = userIds;
+        test.info("Users belonging to Fancode City: " +userIds);
+        Assert.assertFalse(userIds.isEmpty(),"No users found in Fancode city");
 
     }
 
     @Test(priority = 3)
     public void checkUsersTasks(){
+        test = TestReportUtils.createTest("Checking users belonging to Fancode city have more than 50% completed tasks.");
+
         double passingPercentValue = 50;
         List<Integer> ids =typeicodeAPIS.getEligibleUsersIds();
         Map<Integer,Double> userCompletionPercentage = typeicodeAPIS.taskCompletionPercentage(ids);
         userCompletionPercentage.forEach((userId,complPerc)->{
+            test.info("Given user "+ userId +"has completed "+ complPerc+ "tasks");
             Assert.assertTrue( complPerc > passingPercentValue,"User Id"+ userId+"has less than 50% tasks completed,with value:"+complPerc);
         });
     }
 
     @Test(priority = 4)
     public void checkPostsApi(){
+        test = TestReportUtils.createTest("Checking Posts Api");
         Integer respCode = typeicodeAPIS.checkPostsApi();
+
+        test.info("Status Code received:" + respCode);
         Assert.assertEquals(respCode,200,"Posts api connection is stable");
     }
 
     @Test(priority = 4)
     public void checkCommentsApi(){
+        test = TestReportUtils.createTest("Checking Comments Api");
         Integer respCode = typeicodeAPIS.checkCommentsApi();
+        test.info("Status Code received:" + respCode);
         Assert.assertEquals(respCode,200,"Comments api connection is stable");
     }
 
     @Test(priority = 4)
     public void checkAlbumsApi(){
+        test = TestReportUtils.createTest("Checking Albums Api");
         Integer respCode = typeicodeAPIS.checkAlbumsApi();
+        test.info("Status Code received:" + respCode);
         Assert.assertEquals(respCode,200,"Albums api connection is stable");
     }
     @Test(priority = 4)
     public void checkPhotosApi(){
+        test= TestReportUtils.createTest("Checking Photos API");
         Integer respCode = typeicodeAPIS.checkPhotosApi();
+        test.info("Status Code received:" + respCode);
         Assert.assertEquals(respCode,200,"Photos api connection is stable");
     }
-
+    @AfterClass
+    public void tearDownClass() {
+        TestReportUtils.flushReport();
+    }
 
 }
