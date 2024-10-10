622eae085eaa336fc8fb9d6833dc95d58da83443
AkshatChourey <56509631+akshat2812@users.noreply.github.com>
2024-10-10 16:23:31 +0530

    Delete Constants.java

--- Constants.java
+++
@@ -1,8 +0,0 @@
-package constants;
-
-public class Constants {
-    public static final double START_LAT = -40;
-    public static final double END_LAT = 5;
-    public static final double START_LONG = 5;
-    public static final double END_LONG = 100;
-}
